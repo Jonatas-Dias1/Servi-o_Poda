760b75f7682fe6512ce10ae03e90ccbc3fdf0e1e
Jonatas-Dias1 <jonatasdias095@gmail.com>
2025-07-01 04:42:37 -0300

    Atualiza index para serverless, adiciona .gitignore e atualiza dependências

--- api/index.js
+++ api/index.js
@@ -1,24 +1,29 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import routes from '../routes/route.js'; // rotas externas
-import { createServer } from 'http';
+import routes from '../routes/routes.js';
+import mongoose from 'mongoose';
+import serverless from 'serverless-http';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-// Caminho correto das views e public
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Servir arquivos estáticos
 app.use(express.static(join(__dirname, '../public')));
 app.set('views', join(__dirname, '../views'));
 
-// Rotas
-app.use(routes)
-app.listen(3001)
-// Exporta o handler compatível com Vercel
-export default app;
+const url = "mongodb+srv://aluno1:aluno1@cluster0.9fgzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(url)
+  .then(() => console.log("MongoDB conectado com sucesso!"))
+  .catch(err => console.error("Erro ao conectar no MongoDB:", err));
+
+app.use(routes);
+
+// ⚠️ Sem app.listen()
+// Exporta handler pra Vercel
+export const handler = serverless(app);
